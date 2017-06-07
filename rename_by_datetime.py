5436194a4a2083fe4296677a681592557875d81e
Liang Zou <lltous@msn.com>
2017-06-06 22:13:32 -0700

    Fix rename_by_datetime because two pictures may have the same datetime

--- rename_by_datetime.py
+++ rename_by_datetime.py
@@ -32,29 +32,30 @@ def main():
     img = Image.open(os.path.join(folder, eachfile))
     exif_data = img._getexif()
     timestamp = exif_data[36867]
-    try:
-      hash_table[timestamp] = eachfile
-    except:
-      print('Timestamp %s of %s is a duplicate' % (timestamp, eachfile))
+    if timestamp in hash_table:
+      hash_table[timestamp].append(eachfile)
+    else:
+      hash_table[timestamp] = [eachfile]
 
   keys = hash_table.keys()
   keys.sort()
   i = 0
   for k in keys:
-    old_name = hash_table[k].split('.')[0]
-    new_name = '%03d_%s' % (i, old_name)
-    old_jpg_name = '%s.JPG' % old_name
-    new_jpg_name = '%s.JPG' % new_name
-    print ('Rename %s -> %s' % (old_jpg_name, new_jpg_name))
-    old_cr2_name = '%s.CR2' % old_name
-    new_cr2_name = '%s.CR2' % new_name
-    print ('Rename %s -> %s' % (old_cr2_name, new_cr2_name))
-    if not args.dry_run:
-      os.rename(os.path.join(folder, old_jpg_name),
-                os.path.join(folder, new_jpg_name))
-      os.rename(os.path.join(folder, old_cr2_name),
-                os.path.join(folder, new_cr2_name))
-    i += 1
+    for eachfile in hash_table[k]:
+      old_name = eachfile.split('.')[0]
+      new_name = '%03d_%s' % (i, old_name)
+      old_jpg_name = '%s.JPG' % old_name
+      new_jpg_name = '%s.JPG' % new_name
+      print ('Rename %s -> %s' % (old_jpg_name, new_jpg_name))
+      old_cr2_name = '%s.CR2' % old_name
+      new_cr2_name = '%s.CR2' % new_name
+      print ('Rename %s -> %s' % (old_cr2_name, new_cr2_name))
+      if not args.dry_run:
+        os.rename(os.path.join(folder, old_jpg_name),
+                  os.path.join(folder, new_jpg_name))
+        os.rename(os.path.join(folder, old_cr2_name),
+                  os.path.join(folder, new_cr2_name))
+      i += 1
 
 
 if __name__ == '__main__':
