036e91633068f962834bde15bff750ce1d3852a1
Liang Zou <lltous@msn.com>
2019-09-03 21:45:05 -0700

    Make the help string clearer

--- flags.py
+++ flags.py
@@ -3,10 +3,14 @@
 import argparse
 
 common_parser = argparse.ArgumentParser(add_help=False)
-common_parser.add_argument('-f',
-                           '--folder',
-                           type=str,
-                           help='The folder to process')
+common_parser.add_argument(
+    '-f',
+    '--folder',
+    type=str,
+    help=('The folder to process. If a tool accepts wildcards matching '
+          '(e.g. 2019-07*), make sure the string is double quoted, e.g. '
+          '"2019-07*"'))
+
 common_parser.add_argument('--dry_run',
                            dest='dry_run',
                            action='store_true',
